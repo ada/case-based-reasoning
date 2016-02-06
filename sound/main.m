3e38362d93a0e3b0bc1dbf4ac286f8d3bd175c94
Alex Anis <aan12003@gmail.com>
2016-02-06 23:26:33 +0100

    Added feature array

--- sound/main.m
+++ sound/main.m
@@ -1,9 +1,12 @@
 clc; clear all; close all;
 
-% Read sound file
-filename = 'Blåhake.mp3';
+COL_BIRD_NAME = 1; 
+COL_STD = 2;
+COL_MEAN = 3;
+
+% Load sound file
+filename = 'Blåhake.mp3'; 
 [y,Fs] = audioread(filename);
-y0 = y; 
 
 % Convert stereo to mono
 if size(y,2) == 2
@@ -11,77 +14,22 @@ if size(y,2) == 2
 end
 
 % Split into frames
-frameLength = 2; % seconds
+frameLength = 1; % seconds
 frameSize = frameLength * Fs; % frames
-y = y(1 : floor(size(y, 1) / (Fs * frameLength)) * frameSize); % cut to lowest multiple of frameSize
+nFrame = floor(size(y, 1) / (Fs * frameLength)); % total number of frames
+y = y(1 : nFrame * frameSize); % cut to lowest multiple of frameSize
+results = [];
 
-% Extacts features from each frame
 for i = 1 : frameSize : size(y, 1)
-    t = y(i:i + frameSize - 1); 
-    plot(t);
-    sound(t, Fs);
+    % Extacts features for each frame
+    frame = y(i:i + frameSize - 1); 
+    feature = zeros(1);
+    feature(COL_BIRD_NAME) = 1;
+    feature(COL_STD) = std(frame);
+    feature(COL_MEAN) = mean(frame);
+    results = [results; feature]; 
+    plot(frame);
+    sound(frame, Fs);
     waitforbuttonpress;
 end
 
-
-
-
-
-
-%%
-for collection = collections 
-    for person = people
-        personIndex = find(not(cellfun('isempty', strfind(people, char(person))))); 
-        for state = states
-            postfix = ['_',state{1},'_',person{1},'_',collection{1},'.csv'];
-            if size(ls([DIRNAME,'*',postfix])) ~= 0
-                
-                session.HR = csvread([DIRNAME,'HR',postfix]);
-                session.HRV = csvread([DIRNAME,'HRV',postfix]);
-                session.FT = csvread([DIRNAME,'FT',postfix]);
-                session.RR = csvread([DIRNAME,'RR',postfix]);
-                session.SCL = csvread([DIRNAME,'SCL',postfix]);
-                session.Duration = floor(size(session.HR,1)/240);
-                session.State = find(ismember(states,state))-1; 
-       
-                for i = 1 : INTERVAL : session.Duration
-                   A = (i-1) * 240 + 1; % 240 rows = 1 minute
-                   B = A + 240;
-                   row = offset + i; 
-      
-                   [~, m] = freq_mag(session.HR(A:B));
-                   [LF, HF] = freq_LF_HF(m); 
-                   data(row, COL_HR_LF)        = LF;
-                   data(row, COL_HR_HF)        = HF;
-                   data(row, COL_HR_D0_STD)    = std(session.HR(A:B));
-                   data(row, COL_HR_D0_MEAN)   = mean(session.HR(A:B));
-                   data(row, COL_HR_D1_MEAN)   = mean(diff(session.HR(A:B)));
-                   data(row, COL_HRV_D0_STD)   = std(session.HRV(A:B));
-                   data(row, COL_HRV_D0_STD0)  = std(session.HRV);
-                   data(row, COL_HRV_D0_MEAN)  = mean(session.HRV(A:B));
-                   data(row, COL_HRV_D0_MEAN0) = mean(session.HRV);
-                   data(row, COL_HRV_D1_MEAN)  = mean(diff(session.HRV(A:B)));
-                   data(row, COL_FT_D0_STD)    = std(session.FT(A:B));
-                   data(row, COL_FT_D0_MEAN)   = mean(session.FT(A:B));
-                   data(row, COL_FT_D1_MEAN)   = mean(diff(session.FT(A:B)));
-                   data(row, COL_RR_D0_STD)    = std(session.RR(A:B));
-                   data(row, COL_RR_D0_MEAN)   = mean(session.RR(A:B));
-                   data(row, COL_RR_D1_MEAN)   = mean(diff(session.RR(A:B)));
-                   data(row, COL_SCL_D0_MEAN)  = mean(session.SCL(A:B));
-                   data(row, COL_SCL_D0_STD)   = std(session.SCL(A:B));
-                   data(row, COL_ALC)          = session.State;
-                   data(row, COL_MASS)         = mass(personIndex);
-                end
-                offset = size(data,1);
-            end
-        end
-    end
-end
-
-
-data(1:118,:) = Shuffle(data(1:118,:));
-
-%% Export each column to a seperate file
-delete('export/*.csv');
-csvwrite('export/HR_D0_STD.csv',    data(:,COL_HR_D0_STD));
-csvwrite('export/ALC.csv',          data(:,COL_ALC));
