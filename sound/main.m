0749ae14aa5b85b9106c7a7ab7373d26e575e6c2
Alex Anis <aan12003@gmail.com>
2016-02-06 23:58:29 +0100

    fft

--- sound/main.m
+++ sound/main.m
@@ -3,6 +3,7 @@ clc; clear all; close all;
 COL_BIRD_NAME = 1; 
 COL_STD = 2;
 COL_MEAN = 3;
+COL_PEAK1_FREQ = 4; 
 
 % Load sound file
 filename = 'Blåhake.mp3'; 
@@ -23,13 +24,26 @@ results = [];
 for i = 1 : frameSize : size(y, 1)
     % Extacts features for each frame
     frame = y(i:i + frameSize - 1); 
+    
+    % Fourier transform
+    m = length(frame);      % Window length
+    n = pow2(nextpow2(m));  % Transform length
+    b = fft(frame,n);       % DFT of signal
+    f = (0:n-1)*(Fs/n)/10;  % Frequency range
+    p = b.*conj(b)/n;       % Power of the DFT
+    f = f(1:floor(n/2));    % Cut up to the nyquist frequency
+    p = p(1:floor(n/2));    % Cut up to the nyquist frequency
+    
     feature = zeros(1);
     feature(COL_BIRD_NAME) = 1;
     feature(COL_STD) = std(frame);
     feature(COL_MEAN) = mean(frame);
+    feature(COL_PEAK1_FREQ) = f(find(p==max(p)));
     results = [results; feature]; 
-    plot(frame);
+    
+    plot(f,p);
     sound(frame, Fs);
-    waitforbuttonpress;
+    break;
+    %pause;
 end
-
+results
