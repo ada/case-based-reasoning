bf54ec72f664eebe5f9330a29f459096df7c1be8
Johan <johaa1993@gmail.com>
2016-02-11 18:12:47 +0100

    packaging

--- src/main.adb
+++ src/main.adb
@@ -1,101 +1,125 @@
-with Ada.Text_IO; use Ada.Text_IO;
-with Ada.Float_Text_IO; use Ada.Float_Text_IO;
-with Ada.Numerics.Real_Arrays; use Ada.Numerics.Real_Arrays;
+with Ada.Text_IO;
+with Ada.Float_Text_IO;
+with Ada.Numerics.Real_Arrays;
 
 procedure Main is
-   subtype Range_Total is Integer range 1 .. 5; --Number of cases
-   subtype Range_Train is Range_Total range Range_Total'First .. 2; --Number of training rows
-   subtype Range_Eval is Range_Total range Range_Train'Last .. Range_Total'Last; --Number of evaluation rows
-   subtype Vector_Attributes is Real_Vector (1 .. 3); --Feature continer type
-   type Vector_Array is array (Range_Total range <>) of Vector_Attributes;--Case container type
 
-   procedure Read ( X : out Vector_Array; C : Positive; Name : String; Min, Max : out Float ) is
-      F : File_Type;
-   begin
-      Min := Float'Last;
-      Max := Float'First;
-      Open (F, In_File, Name);
-
-      for I in Range_Total loop
-         declare
-            S : String := Get_Line (F);
-            V : Float := Float'Value (S);
-         begin
-            -- Update min max values
-            if V > Max then
-               Max := V;
-            end if;
-            if V < Min then
-               Min := V;
-            end if;
-
-            X (I)(C) := V;
-         end;
-      end loop;
-
-      Close (F);
+   package Samples is
+      use Ada.Numerics.Real_Arrays;
+      subtype Attribute_Type is Integer range 1 .. 3;
+      subtype Sample is Real_Vector (Attribute_Type);
+      type Sample_Array is array (1 .. 5) of Sample;
+      procedure Read (Name : String; A : Attribute_Type; X : out Sample_Array; Min : out Float; Max : out Float);
+      procedure Normalize (A : Attribute_Type; Min, Max : Float; X : in out Sample_Array);
+      procedure Put (X : Sample);
+      function Deviation (W, A, B : Real_Vector) return Float;
    end;
 
-   procedure Put ( X : Real_Vector;  Scientific_Notation : Integer := 0) is
-   begin
-      for E of X loop
-         Put (E, 4, 1, Scientific_Notation);
-      end loop;
+
+
+   package body Samples is
+
+      procedure Read (Name : String; A : Attribute_Type; X : out Sample_Array; Min : out Float; Max : out Float) is
+         use Ada.Text_IO;
+         use Ada.Float_Text_IO;
+         File : File_Type;
+      begin
+         Min := Float'Last;
+         Max := Float'First;
+         Open (File, In_File, Name);
+         for I in X'Range loop
+            declare
+               V : Float renames X (I) (A);
+            begin
+               Get (File, V);
+               if V > Max then
+                  Max := V;
+               end if;
+               if V < Min then
+                  Min := V;
+               end if;
+            end;
+         end loop;
+         Close (File);
+      end;
+
+      function Deviation (W, X1, X2 : Float) return Float is
+      begin
+         return W * abs (X1 - X2);
+      end;
+
+      function Deviation (W, A, B : Real_Vector) return Float is
+         Sum : Float := 0.0;
+      begin
+         for I in A'Range loop
+            Sum := Sum + Deviation (W(I), A (I), B (I));
+         end loop;
+         return Sum;
+      end;
+
+      function Normalize (X : Float; Min : Float; Max : Float) return Float is
+      begin
+         return (X - Min) / (Max - Min);
+      end;
+
+      procedure Normalize (A : Attribute_Type; Min, Max : Float; X : in out Sample_Array) is
+      begin
+         for I in X'Range loop
+            X (I) (A) := Normalize (X (I) (A), Min, Max);
+         end loop;
+      end;
+
+      procedure Put (X : Sample) is
+         use Ada.Float_Text_IO;
+      begin
+         for E of X loop
+            Put (E, 4, 1, 0);
+         end loop;
+      end;
+
    end;
 
-   function Deviation (C, S : Float) return Float is
-   begin
-      return abs (C - S);
-   end;
+   use Samples;
 
-   function Deviation (W, Case_1, Case_2 : Vector_Attributes) return Float is
-      Sum : Float := 0.0;
-   begin
-      for i in Vector_Attributes'First .. Vector_Attributes'Last - 1 loop
-         Sum := Sum + W(i) * Deviation (Case_1(i), Case_2(i));
-      end loop;
-      return Sum;
-   end;
 
-   function Normalize (X : Float; Min : Float; Max : Float) return Float is
-   begin
-      return (X - Min) / (Max - Min);
-   end;
 
-   procedure Normalize ( X : in out Vector_Array; Col : Positive; Min, Max : Float ) is
-   begin
-      for I in X'Range loop
-         X (I) (Col) := Normalize (X(I)(Col), Min, Max);
-      end loop;
-   end;
-
-   C : Vector_Array (Range_Total) := (others => (others => 0.0));
-   W : Vector_Attributes := (Others => 1.0); --Weight array
+   X : Sample_Array;
+   W : Sample := (Others => 1.0);
    Min, Max : Float := 0.0;
 begin
-   Read (C, 1, "A.csv", Min, Max);
-   Normalize (C, 1, Min, Max);
-   Read (C, 2, "B.csv", Min, Max);
-   Normalize (C, 2, Min, Max);
-   Read (C, 3, "R.csv", Min, Max);
+
+   Read ("A.csv", 1, X, Min, Max);
+   Normalize (1, Min, Max, X);
+   Read ("B.csv", 2, X, Min, Max);
+   Normalize (2, Min, Max, X);
+   Read ("R.csv", 3, X, Min, Max);
+   Normalize (3, Min, Max, X);
 
    -- Print Cases with their respective attributes
-   for E of C loop
-      Put(E);
-      New_Line;
+   for I in Sample_Array'Range loop
+      declare
+         use Ada.Text_IO;
+      begin
+         Put (X (I));
+         New_Line;
+      end;
    end loop;
 
 
    -- Calculate deviation between Case 1 and all other cases.
    -- The case with max value is the most similar one.
    declare
-      S : Float := 0.0;
+      use Ada.Float_Text_IO;
+      use Ada.Text_IO;
+      Sum : Float := 0.0;
    begin
-      for i in C'Range loop
+      for I in Sample_Array'Range loop
+         Sum := Deviation (W, X (1) (1 .. 2), X (I) (1 .. 2));
+         Put (Sum, 2, 2, 0);
          New_Line;
-         S := Deviation (W, C(1), C(i));
-         Put(S, 2, 2, 0);
       end loop;
    end;
 
+
 end;
+
